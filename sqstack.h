f8f2862080ee997fd2fb09cad6486f103c946ba4
uTank <hongyt2045@163.com>
2013-12-03 15:28:51 +0800

    Create sqstack.h

---
+++ sqstack.h
@@ -0,0 +1,22 @@
+#ifndef __SqStack_H_
+#define __SqStack_H_
+
+#include "base.h"
+
+typedef int StackElemType;
+
+#define SQ_STACK_MAX_SIZE     (200)
+
+struct t_sqstack;
+
+struct t_sqstack *stack_init(void);
+Status stack_push(struct t_sqstack *pStack, StackElemType e);
+Status stack_pop(struct t_sqstack *pStack, StackElemType *e);
+Status stack_get_top(struct t_sqstack *pStack, StackElemType *e);
+Status stack_empty(struct t_sqstack *pStack);
+void stack_clear(struct t_sqstack *pStack);
+int stack_lenght(struct t_sqstack *pStack);
+void stack_destroy(struct t_sqstack **pStack);
+void stack_traverse(struct t_sqstack *pStack, void (*vi)(StackElemType));
+
+#endif /* __SqStack_H_ */
